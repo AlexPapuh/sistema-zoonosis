6c5c4a4dcb4b2fb343ea7551a1075f960c42f508
Alex Serrudo <alex14.as63@gmail.com>
2026-06-03 20:24:23 -0400

    Aumentar limite de fotos a 50mb

--- backend/server.js
+++ backend/server.js
@@ -44,8 +44,8 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 async function testDbConnection() {
   try {
